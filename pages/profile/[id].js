2e7546bfbdb401d6682189cf1035cd6cd1a71294
wfuller <wfuller87@gmail.com>
2019-09-30 15:07:35 -0700

    Updating profile with new stat component

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,11 +1,54 @@
 import { Fragment } from 'react';
 
+import Link from 'next/link';
+
+import Stat from '../../components/Stat';
+
 const Profile = ({ user: { user, followers } }) => {
+  console.log('user: ', user);
+  console.log('user followers: ', followers);
+
   return (
     <Fragment>
-      <h1>Login: {user.login}</h1>
-      <img src={user.avatar_url} />
-      <p>Location: {user.location}</p>
+      {/* PROFILE HEADER: Image, Username, Bio, Type, Meta data */}
+      <section>
+        <h1>
+          <a href={`https://github.com/${user.login}`}>{user.login}</a>
+        </h1>
+        <img src={user.avatar_url} />
+        <p>{user.bio}</p>
+        {/* meta data */}
+        <ul>
+          <li>Location: {user.location}</li>
+          <li>Company: {user.company}</li>
+          <li>
+            <a href={user.blog}>Website</a>
+          </li>
+        </ul>
+      </section>
+
+      {/* STATS: Followers, Following, Repos, Gists */}
+      <section>
+        <Stat title="Followers" figure={user.followers} />
+        <Stat title="Following" figure={user.following} />
+        <Stat title="Repos" figure={user.public_repos} />
+        <Stat title="Gists" figure={user.public_gists} />
+      </section>
+      {/* Top 30 Following */}
+      <section>
+        <ul>
+          {followers.map(follower => (
+            <li key={follower.id}>
+              <Link href={`/profile/${follower.id}`}>
+                <a>
+                  <img src={follower.avatar_url} />
+                  <p>{follower.login}</p>
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
     </Fragment>
   );
 };
