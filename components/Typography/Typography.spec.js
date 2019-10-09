c00ac390bc4cc33f0ea4c0d84b89dfa15ffbc8b3
Steve <steve.fuller@leafly.com>
2019-10-09 10:54:45 -0700

    updating typography spec

--- components/Typography/Typography.spec.js
+++ components/Typography/Typography.spec.js
@@ -2,11 +2,15 @@ import { shallow, mount } from 'enzyme';
 import Typography from '.';
 
 describe('<Typography />', () => {
-  it('renders H1', () => {
-    const wrapper = render(<Typography variant="h1" />);
-    // console.log(wrapper.debug());
-    const elem = wrapper;
-    console.log(elem.debug());
-    expect(elem.contains('h1')).toEqual(true);
+  it('renders h1', () => {
+    const wrapper = mount(<Typography variant="h1" />);
+    const elem = wrapper.find('h1');
+    expect(elem.length).toBe(1);
+  });
+
+  it('renders h2', () => {
+    const wrapper = mount(<Typography variant="h2" />);
+    const elem = wrapper.find('h2');
+    expect(elem.length).toBe(1);
   });
 });
