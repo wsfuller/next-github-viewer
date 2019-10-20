4e74017a39ba116f4c5254403205db64f7703d89
wfuller <wfuller87@gmail.com>
2019-10-19 22:51:15 -0700

    Removing gridGap function

--- components/Grid/Grid.styles.helpers.js
+++ components/Grid/Grid.styles.helpers.js
@@ -9,22 +9,20 @@ export const rows = rows => css`
   grid-template-rows: ${rows};
 `;
 
-export const gridGaps = (direction, gap) => {
-  if (direction === 'column') {
-    return css`
-      column-gap: ${pxToRem(gap * baseUnit)};
-    `;
-  } else {
-    return css`
-      row-gap: ${pxToRem(gap * baseUnit)};
-    `;
-  }
-};
-
 export const gridBreakpoint = (breakpoint, template, size) => css`
   @media only screen and (min-width: ${breakpoint}px) {
     grid-template-columns: ${template[size].columns};
-    ${template[size].rowGap && gridGaps('row', template[size].rowGap)}
-    ${template[size].columnGap && gridGaps('column', template[size].columnGap)}
+    ${template[size].rowGap &&
+      css`
+        row-gap: ${pxToRem(template[size].rowGap * baseUnit)};
+      `}
+    ${template[size].columnGap &&
+      css`
+        column-gap: ${pxToRem(template[size].columnGap * baseUnit)};
+      `}
+    ${template[size].justifyItems &&
+      css`
+        justify-items: ${template[size].justifyItems};
+      `}
   }
 `;
