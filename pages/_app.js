5a0bd0eee56fa5b0067da68fdfd8968615459ae7
vsanchez1987 <vsanchez1987@yahoo.com>
2019-11-03 12:13:00 -0800

    added favicon and favicon package

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,10 @@ class Application extends App {
             href="https://fonts.googleapis.com/css?family=Lato:400,700|Roboto:300,400,700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="gitview icon"
+            href="./static/favicon.ico"
+          />
         </Head>
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
