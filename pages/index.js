2a4168712e3ed152e0140ef877b50a8785e42fc6
wfuller <wfuller87@gmail.com>
2019-11-24 14:00:03 -0800

    Implement SectionHeader

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import React, { Component, Fragment } from 'react';
 
 import Link from 'next/link';
 import Head from 'next/head';
-import { IoIosMenu } from 'react-icons/io';
 
 import { Container, Row, GridItem, Grid } from '../components/Grid';
 import AppBar from '../components/AppBar';
 import SearchBar from '../components/Search/SearchBar';
+import SectionHeader from '../components/SectionHeader';
 
 class Home extends Component {
   constructor(props) {
@@ -24,18 +24,29 @@ class Home extends Component {
           <title>Home</title>
         </Head>
         <AppBar />
-        <h1>Header</h1>
         <SearchBar />
         {/* Hero Search */}
         <h1>Hero w/ Searchbar Component</h1>
         {/* Section Options */}
-        <h1>Popular Projects/Repos</h1>
-        Next | Bootstrap | freeCodeCamp | You-Dont-Know-JS
-        {/* https://www.freecodecamp.org/news/the-10-github-repos-people-mention-the-most-in-freecodecamps-main-chat-room-189750600fa4/ */}
-        <h1>Developed By Cards:</h1>
-        {/* https://stripe.com/ lifting cards section*/}
-        <h1>Github Docs</h1>
-        {/* https://stripe.com/ right below lifting card section*/}
+        <section>
+          <Container>
+            <SectionHeader title="Popular Projects" />
+            Next | Bootstrap | freeCodeCamp | You-Dont-Know-JS
+            {/* https://www.freecodecamp.org/news/the-10-github-repos-people-mention-the-most-in-freecodecamps-main-chat-room-189750600fa4/ */}
+          </Container>
+        </section>
+        <section>
+          <Container>
+            <SectionHeader title="Developed by" />
+            {/* https://stripe.com/ lifting cards section*/}
+          </Container>
+        </section>
+        <section>
+          <Container>
+            <SectionHeader title="GitHub" />
+            {/* https://stripe.com/ right below lifting card section*/}
+          </Container>
+        </section>
         <h1>Footer</h1>
         {/* <Footer></Footer> */}
       </Fragment>
