f47c36395ac952e564cda2c41e5dd9b5e400c875
wfuller <wfuller87@gmail.com>
2019-11-24 14:20:58 -0800

    Add Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import App, { Container } from 'next/app';
 
 import { theme } from '../components/_theme';
+import Layout from '../components/Layout';
 
 class Application extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -34,21 +35,33 @@ class Application extends App {
             href="https://fonts.googleapis.com/css?family=Lato:400,700|Roboto:300,400,700&display=swap"
             rel="stylesheet"
           />
+          <link rel="gitview icon" href="./static/favicon.ico" />
           <link
-            rel="gitview icon"
-            href="./static/favicon.ico"
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="./favicon-package/apple-touch-icon.png"
           />
-          <link rel="apple-touch-icon" sizes="180x180" href="./favicon-package/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="./favicon-package/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="./favicon-package/favicon-16x16.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="./favicon-package/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="./favicon-package/favicon-16x16.png"
+          />
           <link rel="manifest" href="./favicon-package/site.webmanifest" />
           <link rel="mask-icon" href="./favicon-package/safari-pinned-tab.svg" color="#5bbad5" />
-          <meta name="msapplication-TileColor" content="#ffffff"/>
+          <meta name="msapplication-TileColor" content="#ffffff" />
           <meta name="theme-color" content="#ffffff"></meta>
-
         </Head>
         <ThemeProvider theme={theme}>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </ThemeProvider>
       </Fragment>
     );
