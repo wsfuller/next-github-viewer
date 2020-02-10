450e37b7518b29c0b5cfdf5a1bab0638a0e7cbc9
wfuller <wfuller87@gmail.com>
2020-02-09 22:10:46 -0800

    Implement React modal

--- components/AppBar/AppBarMenu.js
+++ components/AppBar/AppBarMenu.js
@@ -1,9 +1,10 @@
 import React, { Fragment, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-
 import { IoIosClose } from 'react-icons/io';
+import Modal from 'react-modal';
 
+import Logo from '../../static/logo-text-icon.svg';
 import {
   StyledAppBarMenuOverlay,
   StyledAppBarMenu,
@@ -11,10 +12,45 @@ import {
   StyledAppBarMenuButton
 } from './AppBar.styles';
 import Image from '../Image';
+import { List, ListItem } from '../List';
 
-import Logo from '../../static/logo-text-icon.svg';
+const customStyles = {
+  overlay: {
+    zIndex: 10000
+  }
+};
 
 const AppBarMenu = ({ menu, toggleMenu }) => {
+  const [modal, setModal] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+  const menuLinks = [
+    {
+      text: 'About',
+      href: '#'
+    },
+    {
+      text: 'Technology',
+      href: '#'
+    },
+    {
+      text: 'GitHub Repo',
+      href: '#'
+    }
+  ];
+
+  function handleModal(whichModal) {
+    setModal(true);
+    setModalContent(whichModal);
+  }
+
+  useEffect(() => {
+    Modal.setAppElement('#__next');
+    if (modal) {
+      // set modal content here
+      console.log('use effect modal: ', modalContent.toLowerCase());
+    }
+  }, [modal]);
+
   return (
     <Fragment>
       <StyledAppBarMenu showMenu={menu}>
@@ -28,9 +64,19 @@ const AppBarMenu = ({ menu, toggleMenu }) => {
             <IoIosClose />
           </StyledAppBarMenuButton>
         </StyledMenuHeader>
-        This is the app menu
+        <List>
+          {menuLinks.map((link, i) => (
+            <ListItem key={`menu-link-${i}`} hover onClick={() => handleModal(link.text)}>
+              {link.text}
+            </ListItem>
+          ))}
+        </List>
       </StyledAppBarMenu>
       <StyledAppBarMenuOverlay showMenu={menu} />
+      <Modal isOpen={modal} style={customStyles} contentLabel={modalContent}>
+        Modal Content for: {modalContent}
+        <button onClick={() => setModal(false)}>close</button>
+      </Modal>
     </Fragment>
   );
 };
